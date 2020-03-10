f78024f76887788204b2f37e627dd0cd5e6fa33c
Fethia <fethia.belkilani@gmail.com>
2020-03-10 14:40:52 +0100

    first

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProjectController } from './project/project.controller';
+import { TimeController } from './time/time.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, ProjectController, TimeController],
   providers: [AppService],
 })
 export class AppModule {}
