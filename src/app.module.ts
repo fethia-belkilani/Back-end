b094ebdcd0165f43a9a9b3b19014e0c6325bd53d
Fethia <fethia.belkilani@gmail.com>
2020-03-10 14:53:54 +0100

    third

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProjectController } from './project/project.controller';
 import { TimeController } from './time/time.controller';
+import { UserController } from './user/user.controller';
+import { ActiveDirectoryController } from './active-directory/active-directory.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, ProjectController, TimeController],
+  controllers: [AppController, ProjectController, TimeController, UserController, ActiveDirectoryController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/time/time.controller.ts
+++ src/time/time.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Query, Delete, Res, HttpStatus } from '@nestjs/common';
+import Redmine = require('node-redmine');
+import { Response } from 'express';
+
 
 @Controller('time')
-export class TimeController {}
+export class TimeController {
+
+  @Get()
+  async getTimes(@Res() res: Response) {
+    //const books = await this.booksService.getBooks();
+    //return books;
+
+    const hostname = process.env.REDMINE_HOST || 'http://localhost:3000';
+    const config = {
+      apiKey: process.env.REDMINE_APIKEY || '12b77ffcd93f1259578bd20fe35b3b50480aa27e',  format: 'json'
+
+    };
+    const redmine = new Redmine(hostname, config);
+    redmine.time_entries({include: "id, project_id, hours"}, (err, data) => {
+      if (err) throw err;
+      //console.log(data);
+      res.status(HttpStatus.OK).json(data);
+    });
+  }
+
+}
